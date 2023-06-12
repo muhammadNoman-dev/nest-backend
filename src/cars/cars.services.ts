7b8cffb00b7cbd9f5d1c385aef34ffdca75a079e
m-noman <muhammadnoman8484@gmail.com>
2023-06-12 09:50:14 +0500

    category populated

--- src/cars/cars.services.ts
+++ src/cars/cars.services.ts
@@ -1,20 +1,20 @@
-import { Injectable,NotFoundException } from "@nestjs/common"
+import { Injectable, NotFoundException } from "@nestjs/common"
 import { InjectModel } from "@nestjs/mongoose"
 import { Car, CarDocument } from "src/schemas/car.schema"
 import { CreateCarDto, GetCarDto, UpdateCarDto } from "./cardto/car.dto"
 import { Category } from "src/schemas/category.schema"
-import  { Model, Types } from "mongoose"
+import { Model, Types } from "mongoose"
 
 @Injectable()
 export class CarsService {
-	constructor(@InjectModel(Car.name) private readonly carModel: Model<CarDocument>) {}
+	constructor(@InjectModel(Car.name) private readonly carModel: Model<CarDocument>) { }
 
 	findById(id: string) {
 		return this.carModel.findById(id).lean().exec()
 	}
 
 	getAll() {
-		return this.carModel.find().populate({ path:"category", model: Category.name, select:{ name:1 }, transform : doc=> doc.name }).exec()
+		return this.carModel.find().populate({ path: "category", model: Category.name, select: { name: 1 }, transform: doc => doc.name }).exec()
 	}
 
 	findOne(filters: Partial<Car>) {
@@ -22,25 +22,26 @@ export class CarsService {
 	}
 
 	async create(car: CreateCarDto) {
-		const createdCar = await new this.carModel(car).save()
+		// const createdCar = (await new this.carModel(car).save())
+		const createdCar = await new this.carModel(car).save().then(t => t.populate({ path: "category", model: Category.name, select: { name: 1 }, transform: doc => doc.name }))
 		return createdCar.toObject()
 	}
 
-	async delete(id: string| string[]) {
-		if (Array.isArray(id)){
+	async delete(id: string | string[]) {
+		if (Array.isArray(id)) {
 			const deletedCars = await this.carModel.deleteMany(id)
-		}else {
+		} else {
 			const deletedCar = await this.carModel.findOneAndDelete({ _id: new Types.ObjectId(id) })
 			return deletedCar
 		}
 	}
 
-	async deleteCarsFromCategories(categoryId : string) {
-		const categoryCars =  await this.carModel.find({category : categoryId})
+	async deleteCarsFromCategories(categoryId: string) {
+		const categoryCars = await this.carModel.find({ category: categoryId })
 
-		const ids : string[] = categoryCars.map(cars => cars._id)
-		const deletedCars = await this.carModel.deleteMany({id : { $in: ids }})
-		
+		const ids: string[] = categoryCars.map(cars => cars._id)
+		const deletedCars = await this.carModel.deleteMany({ id: { $in: ids } })
+
 		if (!!deletedCars.deletedCount) return ids
 	}
 
@@ -48,7 +49,7 @@ export class CarsService {
 		const carFound = await this.findById(id)
 
 		if (!carFound) throw new NotFoundException("Not Found")
-		const updatedCar = await this.carModel.findByIdAndUpdate(new Types.ObjectId(id), { $set: car }, { new: true })
+		const updatedCar = await this.carModel.findByIdAndUpdate(new Types.ObjectId(id), { $set: car }, { new: true }).populate({ path: "category", model: Category.name, select: { name: 1 }, transform: doc => doc.name })
 
 		const carObject = updatedCar?.toObject()
 		return carObject
